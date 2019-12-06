5a03008b7a1d17509f544d5e239268031cf6e9bb
ashutri <31681165+ashutri@users.noreply.github.com>
2019-12-07 00:42:39 +0530

    Update Validation_test.go

--- Validation_test.go
+++ Validation_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 func TestValidation(t *testing.T) {
+	//test case to check pass
 	test := "379354508162306"
 
 	if !Validation(test) {
@@ -12,6 +13,21 @@ func TestValidation(t *testing.T) {
 	}
 
 	test = "4388576018402626"
+
+	if Validation(test) {
+		t.Errorf("Expected ”%s” to be invalid according to the Luhn algorithm", test)
+	}
+	
+	
+	//test case to check fail
+	
+	test = ""
+
+	if Validation(test) {
+		t.Errorf("Expected ”%s” to be invalid according to the Luhn algorithm", test)
+	}
+	
+	test = "4388576018400000000000000"
 
 	if Validation(test) {
 		t.Errorf("Expected ”%s” to be invalid according to the Luhn algorithm", test)
